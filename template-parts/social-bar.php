375a8a56cc75f03126ee93d88ee8ec3ecb1a9f36
damiandominella <damiandominella@gmail.com>
2019-09-01 14:02:32 +0200

    fixes

--- template-parts/social-bar.php
+++ template-parts/social-bar.php
@@ -1,12 +1,12 @@
 <!-- Social bar -->
 <div class="topbar">
 	<ul class="socials">
-        <li><a href="https://www.facebook.com/InformaticaapplicataUniversitaDegliStudiDiUrbino?ref=hl"><i class="fab fa-facebook-f"></i></a></li>
-        <li><a href="https://twitter.com/InfoAppl"><i class="fab fa-twitter"></i></a></li>
+        <li><a target="_blank" href="https://www.facebook.com/InfoAppl/"><i class="fab fa-facebook-f"></i></a></li>
+        <li><a target="_blank" href="https://twitter.com/InfoAppl"><i class="fab fa-twitter"></i></a></li>
         <li><a href="skype:$cdl.informatica?call"><i class="fab fa-skype"></i></a></li>
         <!-- <li><a href="#"><i class="fab fa-foursquare"></i></a></li> -->
-        <li><a href="http://mail.google.com/a/campus.uniurb.it"><i class="fas fa-envelope"></i></a></li>
-        <li><a href="<?php bloginfo('rss2_url'); ?>"><i class="fas fa-rss"></i></a></li>
+        <li><a target="_blank" href="http://mail.google.com/a/campus.uniurb.it"><i class="fas fa-envelope"></i></a></li>
+        <li><a target="_blank" href="<?php bloginfo('rss2_url'); ?>"><i class="fas fa-rss"></i></a></li>
     </ul>
 </div>
 <!-- Social bar -->
